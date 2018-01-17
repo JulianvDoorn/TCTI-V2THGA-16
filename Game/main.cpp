ae4fc00417358aa518bfbace576297da2d8b79b0
Julian van Doorn <gigajulian@gmail.com>
2018-01-17 17:23:45 +0100

    Updated main.hpp

--- Game/main.cpp
+++ Game/main.cpp
@@ -6,6 +6,7 @@
 #include "Ball.hpp"
 #include "Player.hpp"
 #include "EventHandler.hpp"
+#include "Rectangle.hpp"
 
 std::ostream& operator<<(std::ostream& os, sf::Vector2f v) {
 	os << v.x;
@@ -15,58 +16,50 @@ std::ostream& operator<<(std::ostream& os, sf::Vector2f v) {
 }
 
 int main() {
-	const float FPS = 60.0f; // The desired FPS. (The number of updates each second).
-	bool redraw = true;      // Do I redraw everything on the screen?
-
-	std::vector<Drawable*> drawables;
-	sf::View view2(sf::Vector2f(150, 10), sf::Vector2f(150, 450));
-
+	const float FPS = 60.0f;
+	
 	sf::RenderWindow window(sf::VideoMode(300, 900, 32), "Hello");
 	window.setFramerateLimit((int) FPS);
 	sf::Clock clock;
 
-	Ball ball = Ball();
-	ball.setRadius(10);
-	ball.setPosition({ 150, 500 });
-	ball.setFillColor(sf::Color(255, 0, 0));
 
 
-	Player player = Player(view2, window);
 
-	EventHandler eventHandler;
+	Rectangle rectangle;
+	rectangle.setSize({ 100, 50 });
+	rectangle.setPosition({ 100, 100 });
 
-	eventHandler.BindPhysicsObject(&ball);
 
-	eventHandler.BindFunction(EventType::KeyDown, EventFunction([](const EventSignal& signal) {
+
+	EventHandler& eventHandler0 = EventHandler::newEventHandler();
+	eventHandler0.bindPhysicsObject(rectangle);
+
+	EventHandler& eventHandler1 = EventHandler::newEventHandler();
+
+	//std::cout << &eventHandler0 << std::endl;
+	//std::cout << &eventHandler1 << std::endl;
+
+	eventHandler0.bindFunction(EventType::KeyDown, EventFunction([](const EventSignal& signal) {
 		KeyCodeArguments arguments = signal.getArguments();
-
-		std::cout << (char) ('a' + arguments.key) << std::endl;
+		std::cout << "event handler 0" << std::endl;
 	}));
 
-	eventHandler.BindFunction(EventType::MouseLeftPressed, EventFunction([](const EventSignal& signal) {
-		MouseClickArguments arguments = signal.getArguments();
-
-		std::cout << "MouseLeftPressed!\n";
-		std::cout << arguments.position.x << ", " << arguments.position.y << std::endl;
-	}));
-
-	eventHandler.BindFunction(EventType::MouseLeftReleased, EventFunction([](const EventSignal& signal) {
-		MouseClickArguments arguments = signal.getArguments();
-
-		std::cout << "Mouse left released!\n";
-		std::cout << arguments.position.x << ", " << arguments.position.y << std::endl;
+	eventHandler1.bindFunction(EventType::KeyDown, EventFunction([](const EventSignal& signal) {
+		KeyCodeArguments arguments = signal.getArguments();
+		std::cout << "event handler 1" << std::endl;
 	}));
 
 	sf::Event ev;
 
 	while (window.isOpen()) {
-		// Wait until 1/60th of a second has passed, then update everything.
 		float elapsedTime = clock.getElapsedTime().asSeconds();
 
-		// Handle input
 		while (window.pollEvent(ev)) {
-			if (ev.type == sf::Event::Closed) window.close();
-			eventHandler.HandleSFMLEvent(ev);
+			if (ev.type == sf::Event::Closed) {
+				window.close();
+			}
+
+			EventHandler::handleSFMLEvent(ev);
 		}
 
 		if (elapsedTime >= 1.0f / FPS) {
@@ -76,9 +69,8 @@ int main() {
 
 			window.clear(sf::Color(0, 0, 0));
 			
-			ball.draw(window);
-			player.draw(window);
-			player.update(elapsedTime);
+			rectangle.draw(window);
+
 			window.display();
 		}
 	}
