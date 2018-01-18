c7fe6be275531f0ca4e75fce22cc15019c7cce81
Julian van Doorn <gigajulian@gmail.com>
2018-01-18 20:05:31 +0100

    Added default constructor

--- Game/Button.hpp
+++ Game/Button.hpp
@@ -15,18 +15,9 @@ private:
     Label textLabel;
 
 	Mouse &mouse;
-public:
-	EventSource<> buttonPressed;
-	EventSource<> buttonReleased;
+	bool mouseInside = false;
 
-    Button(sf::Vector2f backgroundSize, sf::Vector2f position, int size, std::string text, sf::Font &font, Mouse &mouse):
-    backgroundSize(backgroundSize), position(position),textLabel(text,font,position,size), mouse(mouse) {
-        background.setSize(backgroundSize);
-        background.setPosition(position);
-		background.setFillColor(sf::Color(0, 153, 51));
-        sf::FloatRect textRectangle= textLabel.getBounds();
-        textLabel.setOrigin(textRectangle.width / 2, textRectangle.height / 2);
-
+	void bindEvents() {
 		mouse.mouseLeftButtonDown.connect([this](const sf::Vector2i mousePos) {
 			if (background.contains(static_cast<sf::Vector2f>(mousePos))) {
 				buttonPressed.fire();
@@ -39,9 +30,70 @@ public:
 			}
 		});
 
+		mouse.mouseMoved.connect([this](const sf::Vector2i mousePos) {
+			if (background.contains(static_cast<sf::Vector2f>(mousePos))) {
+				if (mouseInside == false) {
+					mouseInside = true;
+					mouseEnter.fire();
+				}
+			}
+			else {
+				if (mouseInside == true) {
+					mouseInside = false;
+					mouseLeave.fire();
+				}
+			}
+		});
+	}
+
+public:
+	static sf::Font& getDefaultFont() {
+		static sf::Font font;
+		return font;
+	}
+
+	static void setDefaultFont(const std::string& dir) {
+		getDefaultFont().loadFromFile(dir);
+	}
+
+	EventSource<> buttonPressed;
+	EventSource<> buttonReleased;
+	EventSource<> mouseEnter;
+	EventSource<> mouseLeave;
+
+	Button(Mouse& mouse) : mouse(mouse), textLabel(getDefaultFont()) {
+		setFont(getDefaultFont());
+		bindEvents();
+	}
+
+    Button(sf::Vector2f backgroundSize, sf::Vector2f position, int size, std::string text, sf::Font& font, Mouse& mouse):
+		backgroundSize(backgroundSize),
+		position(position),
+		textLabel(text, font, position, size),
+		mouse(mouse)
+	{
+        background.setSize(backgroundSize);
+        background.setPosition(position);
+		background.setFillColor(sf::Color(0, 153, 51));
+        sf::FloatRect textRectangle = textLabel.getBounds();
+        textLabel.setOrigin(textRectangle.width / 2, textRectangle.height / 2);
+
+		bindEvents();
     };
 
-    void setText(std::string text){
+	void setSize(sf::Vector2f size) {
+		background.setSize(size);
+	}
+
+	void setCharSize(int32_t size) {
+		textLabel.setCharSize(size);
+	}
+
+	void setFont(sf::Font& font) {
+		textLabel.setFont(font);
+	}
+
+    void setText(const std::string& text){
         textLabel.setText(text);
         sf::FloatRect textRectangle= textLabel.getBounds();
 		textLabel.setOrigin(textRectangle.width / 2, textRectangle.height / 2);
@@ -68,11 +120,7 @@ public:
 		return background.getSize();
 	}
 
-	void setCharacterSize(int size) {
-		textLabel.setCharSize(size);
-	}
-
-    void draw(sf::RenderWindow &window){
+    void draw(sf::RenderWindow& window) {
         background.draw(window);
         textLabel.draw(window);
     }
