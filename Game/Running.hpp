5de40b52313403ae7e8f042151f80fa3f4001035
Julian van Doorn <gigajulian@gmail.com>
2018-01-19 16:00:11 +0100

    Added CollisionGroup in favor of CollisionObjects

--- Game/Running.hpp
+++ Game/Running.hpp
@@ -16,11 +16,12 @@ class Running : public State {
 
 	Player player;
 	Antagonist death;
-	CollisionObjects objects;
+	CollisionGroup collisionGroup;
 
 	Rectangle floor0;
 	Rectangle floor1;
 	Rectangle wall;
+	Rectangle wall1;
 
 	bool gameOver = false;
 	float gameOverCounter = 3.0f;
@@ -30,21 +31,26 @@ public:
 		State("running"),
 		statemachine(statemachine),
 		focus(statemachine.window),
-		player(statemachine.window)
+		player(statemachine.window),
+		collisionGroup(player)
 	{
-		objects.add(death);
-		
+		collisionGroup.add(death);
+
 		floor0.setSize({ 600, 100 });
 		floor0.setPosition({ 0, 600 });
-		objects.add(floor0);
+		collisionGroup.add(floor0);
 
 		floor1.setSize({ 60, 10 });
 		floor1.setPosition({ 0, 500 });
-		objects.add(floor1);
+		collisionGroup.add(floor1);
 
 		wall.setSize({ 20, 50 });
 		wall.setPosition({ 250, 550 });
-		objects.add(wall);
+		collisionGroup.add(wall);
+
+		wall1.setSize({ 20, 100 });
+		wall1.setPosition({ -200, 450 });
+		collisionGroup.add(wall1);
 
 		statemachine.addState(*this);
 	}
@@ -52,6 +58,12 @@ public:
 	void entry() override {
 		focus.setFocus(player);
 		focus.update();
+
+		player.collided.connect([this](Collidable& other) {
+			if (other == death) {
+				game.died.fire();
+			}
+		});
 
 		keyReleasedConnection = game.keyboard.keyReleased.connect([this](sf::Keyboard::Key key) {
 			if (key == sf::Keyboard::Key::Escape) {
@@ -96,7 +108,7 @@ public:
 
 		death.update(elapsedTime);
 
-		player.detectCollision(objects);
+		collisionGroup.resolveCollisions();
 
 		//player.resolveCollision(floor0);
 		//player.resolveCollision(floor1);
@@ -109,6 +121,7 @@ public:
 		floor0.draw(statemachine.window);
 		floor1.draw(statemachine.window);
 		wall.draw(statemachine.window);
+		wall1.draw(statemachine.window);
 
 		focus.update();
 	}
