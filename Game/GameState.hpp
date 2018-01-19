b1752c63f6d901fec329d7ba105af8f4b549fc65
Julian van Doorn <gigajulian@gmail.com>
2018-01-19 13:49:00 +0100

    Added doxygen comments

--- Game/GameState.hpp
+++ Game/GameState.hpp
@@ -1,16 +1,82 @@
 #pragma once
 
+/**
+ * @class	GameState
+ *
+ * @brief	A game state interface.
+ *
+ * @author	Julian
+ * @date	2018-01-19
+ */
+
 class GameState {
+	/** @brief	Name of the State, used for doing transitions towards this state */
 	const std::string name;
 public:
 
+	/**
+	 * @fn	GameState::GameState(const std::string& name) : name(std::move(name))
+	 *
+	 * @brief	Constructor
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @param	name	Name of the state
+	 */
+
 	GameState(const std::string& name) : name(std::move(name)) { }
+
+	/**
+	 * @fn	virtual void GameState::update(const float elapsedTime) = 0;
+	 *
+	 * @brief	Runs an update cycle for this state
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @param	elapsedTime	The elapsed time.
+	 */
 
 	virtual void update(const float elapsedTime) = 0;
 
+	/**
+	 * @fn	virtual void GameState::entry()
+	 *
+	 * @brief	Entry is called once the state has been entered.
+	 * 			The update function is only called after this function has returned.
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 */
+
 	virtual void entry() { };
 
+	/**
+	 * @fn	virtual void GameState::exit()
+	 *
+	 * @brief	Exit is called once the state has been left.
+	 * 			The update function is only called before this function is invoked.
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 */
+
 	virtual void exit() { };
+
+	/**
+	 * @fn	friend bool GameState::operator==(GameState* state, const std::string& name)
+	 *
+	 * @brief	Equality operator.
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @param [in,out]	state	If non-null, the first instance to compare.
+	 * @param 		  	name 	The second instance to compare.
+	 *
+	 * @return	state->name == name
+	 */
 
 	friend bool operator==(GameState* state, const std::string& name) {
 		return state->name == name;
--- Game/Statemachine.hpp
+++ Game/Statemachine.hpp
@@ -7,6 +7,15 @@
 #include "GameState.hpp"
 #include "Events.hpp"
 
+/**
+ * @class	InvalidStateException
+ *
+ * @brief	Exception for when a transition is requested to an invalid state. Usually non-existent states.
+ *
+ * @author	Julian
+ * @date	2018-01-19
+ */
+
 class InvalidStateException : public std::exception {
 public:
 	const char* what() const noexcept override {
@@ -15,17 +24,57 @@ public:
 };
 
 class Statemachine {
+	/** @brief	Available states to do transitions towards. */
 	std::vector<GameState*> states;
+
+	/** @brief	The current state. */
 	GameState* currentState = nullptr;
 
 public:
+	/** @brief	The window to render to. */
 	sf::RenderWindow& window;
 
+	/**
+	 * @fn	Statemachine::Statemachine(sf::RenderWindow& window)
+	 *
+	 * @brief	Constructor
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @param [in,out]	window	The render window to apply rendering to.
+	 */
+
 	Statemachine(sf::RenderWindow& window) : window(window) { }
+
+	/**
+	 * @fn	void Statemachine::addState(GameState& gameState)
+	 *
+	 * @brief	Adds a state to Statemachine::states
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @param [in,out]	gameState	State of the game.
+	 */
 
 	void addState(GameState& gameState) {
 		states.push_back(&gameState);
 	}
+
+	/**
+	 * @fn	void Statemachine::doTransition(const std::string& name)
+	 *
+	 * @brief	Executes a transition to the state with the specified name.
+	 * 			Executes GameState::exit() on the leaving state and GameState::entry() on the entering state.
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @exception	InvalidStateException	Thrown when a transition is requested to an invalid state. Usually non-existent states.
+	 *
+	 * @param	name	The name of the state to transist to.
+	 */
 
 	void doTransition(const std::string& name) {
 		if (currentState != nullptr) {
@@ -43,6 +92,17 @@ public:
 		currentState->entry();
 	}
 
+	/**
+	* @fn	virtual void GameState::update(const float elapsedTime) = 0;
+	*
+	* @brief	Runs an update cycle for the current state
+	*
+	* @author	Julian
+	* @date	2018-01-19
+	*
+	* @param	elapsedTime	The elapsed time.
+	*/
+	
 	void update(const float elapsedTime) {
 		window.clear(sf::Color(0, 0, 0));
 		currentState->update(elapsedTime);
