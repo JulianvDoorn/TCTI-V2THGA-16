826ff782dd4b61915f5e42e7bb984dd3719d8838
Julian van Doorn <gigajulian@gmail.com>
2018-01-19 11:49:00 +0100

    Documented using Doxygen

--- Game/SFMLEventDecoder.hpp
+++ Game/SFMLEventDecoder.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
 class SFMLEventDecoder {
+
+	/**
+	 * @fn	virtual void SFMLEventDecoder::decodeSFMLEvent(sf::Event ev) const = 0;
+	 *
+	 * @brief	Decode sfml event into an EventSource signal.
+	 *
+	 * @author	Julian
+	 * @date	2018-01-19
+	 *
+	 * @param	ev	SFML event to decode into an EventSource signal.
+	 */
+
 	virtual void decodeSFMLEvent(sf::Event ev) const = 0;
 };
