322749b186084ddccb373ed975bcecde581c0de6
Julian van Doorn <gigajulian@gmail.com>
2018-01-22 16:12:41 +0100

    Added doxygen for added methods

--- Game/Statemachine.hpp
+++ Game/Statemachine.hpp
@@ -24,6 +24,18 @@ public:
 	}
 };
 
+/**
+ * @class	Statemachine
+ *
+ * @brief	Game statemachine.
+ * 			Allows for transitioning between states.
+ * 			Calls entry and exit on the related states.
+ * 			Also allows for completely resetting states using destructors and constructors.
+ *
+ * @author	Julian
+ * @date	2018-01-22
+ */
+
 class Statemachine {
 	/** @brief	Available states to do transitions towards. */
 	std::map<std::string, std::unique_ptr<State>> states;
@@ -31,8 +43,22 @@ class Statemachine {
 	/** @brief	The current state. */
 	State* currentState = nullptr;
 
-public:
+	/** @brief	The state factory. Used for constructing states by name. */
 	BaseFactory<State, std::string> stateFactory;
+
+public:
+
+	/**
+	 * @fn	template<class T> void Statemachine::registerState(const std::string& name)
+	 *
+	 * @brief	Registers state class T with a name
+	 *
+	 * @author	Julian
+	 * @date	2018-01-22
+	 *
+	 * @tparam	T	Generic type parameter.
+	 * @param	name	The name of T
+	 */
 
 	template<class T>
 	void registerState(const std::string& name) {
@@ -73,6 +99,21 @@ public:
 	void addState(const std::string& name, State& gameState) {
 		states[name] = std::move(std::unique_ptr<State>(&gameState));
 	}
+
+	/**
+	 * @fn	void Statemachine::resetState(const std::string& name)
+	 *
+	 * @brief	Resets the state identified by name.
+	 * 			The reset is done using the stateFactory.
+	 * 			This is done by destructing the state and constructing a new one and update the pointers.
+	 *
+	 * @author	Julian
+	 * @date	2018-01-22
+	 *
+	 * @exception	InvalidStateException	Thrown when a transition is requested to an invalid state. Usually non-existent states.
+	 *
+	 * @param	name	The name of the state to reset.
+	 */
 
 	void resetState(const std::string& name) {
 		auto it = states.find(name); // requires: operator==(std::unique_ptr<State>&, const std::string&)
