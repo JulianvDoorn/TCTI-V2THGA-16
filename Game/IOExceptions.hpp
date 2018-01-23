3dd427f714b00dfb395d09b4ece5d79ec5433ab3
Julian van Doorn <gigajulian@gmail.com>
2018-01-23 16:00:32 +0100

    Added IOExceptions.hpp

---
+++ Game/IOExceptions.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+class BodyFactoryException : public std::exception {
+	std::string err;
+	
+public:
+	explicit BodyFactoryException(std::string s) : err(std::move(s)) { }
+	
+	const char* what() const noexcept final {
+		return err.c_str();
+	}
+};
+
+class UnknownTypeException : public BodyFactoryException {
+public:
+	explicit UnknownTypeException(const std::string& type) : BodyFactoryException(std::string("Unknown type [") + type + "]") { }
+};
+
+class EOFException : public BodyFactoryException {
+public:
+	EOFException() : BodyFactoryException("End of file reached") { }
+	
+	explicit EOFException(const std::string& s) : BodyFactoryException(s + " end of file reached") { }
+};
+
+class SyntaxException : public BodyFactoryException {
+public:
+	explicit SyntaxException(const std::string& s) : BodyFactoryException(s) { }
+};
+
+class UnexpectedValueException : public BodyFactoryException {
+public:
+	explicit UnexpectedValueException(int64_t position, const std::string& unexpectedSymbol) : BodyFactoryException(std::string("Unexpected value at [") + std::to_string(position) + "] got: " + unexpectedSymbol) { }
+};
+
+class OpeningBracketMissingException : public SyntaxException {
+public:
+	explicit OpeningBracketMissingException(int64_t position) : SyntaxException(std::string("Opening bracket missing at [") + std::to_string(position) + "]") { }
+	explicit OpeningBracketMissingException(int64_t position, const std::string& unexpectedSymbol) : SyntaxException(std::string("Opening bracket missing at [") + std::to_string(position) + "] unexpected symbol: " + unexpectedSymbol) { }
+};
+
+class ClosingBracketMissingException : public SyntaxException {
+public:
+	explicit ClosingBracketMissingException(int64_t position) : SyntaxException(std::string("Closing bracket missing at [") + std::to_string(position) + "]") { }
+	explicit ClosingBracketMissingException(int64_t position, const std::string& unexpectedSymbol) : SyntaxException(std::string("Closing bracket missing at [") + std::to_string(position) + "] unexpected symbol: " + unexpectedSymbol) { }
+};
+
+class CommaMissingException : public SyntaxException {
+public:
+	explicit CommaMissingException(int64_t position) : SyntaxException(std::string("Comma missing at [") + std::to_string(position) + "]") { }
+	explicit CommaMissingException(int64_t position, const std::string& unexpectedSymbol) : SyntaxException(std::string("Comma missing at [") + std::to_string(position) + "] unexpected symbol: " + unexpectedSymbol) { }
+};
+
+class QuoteMissingException : public SyntaxException {
+public:
+	explicit QuoteMissingException(int64_t position) : SyntaxException(std::string("Quote missing at [") + std::to_string(position) + "]") { }
+	explicit QuoteMissingException(int64_t position, const std::string& unexpectedSymbol) : SyntaxException(std::string("Quote missing at [") + std::to_string(position) + "] unexpected symbol: " + unexpectedSymbol) { }
+};
+
+class HashtagMissingException : public SyntaxException {
+public:
+	explicit HashtagMissingException(int64_t position) : SyntaxException(std::string("Quote missing at [") + std::to_string(position) + "]") { }
+	explicit HashtagMissingException(int64_t position, const std::string& unexpectedSymbol) : SyntaxException(std::string("Quote missing at [") + std::to_string(position) + "] unexpected symbol: " + unexpectedSymbol) { }
+};
