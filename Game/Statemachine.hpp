a69c8d7ea03b9dcb77caea2e3caefe93e1104bfa
Julian van Doorn <gigajulian@gmail.com>
2018-01-23 09:50:33 +0100

    Added StatemachineException

--- Game/Statemachine.hpp
+++ Game/Statemachine.hpp
@@ -9,6 +9,27 @@
 #include "BaseFactory.hpp"
 
 /**
+* @class	StatemachineException
+*
+* @brief	Exception for signalling statemachine errors.
+*
+* @author	Julian
+* @date	2018-01-22
+*/
+
+class StatemachineException : public std::exception {
+	std::string err;
+
+protected:
+	StatemachineException(const std::string& err) : err(std::move(err)) { }
+
+public:
+	virtual const char* what() const noexcept override {
+		return err.c_str();
+	}
+};
+
+/**
  * @class	InvalidStateException
  *
  * @brief	Exception for when a transition is requested to an invalid state. Usually non-existent states.
