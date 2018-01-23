b4c83f2799cdb9c1f9ab729fb758bdfa5946c435
Julian van Doorn <gigajulian@gmail.com>
2018-01-23 10:00:41 +0100

    Added EOF warning footer for generalising include files

--- Game/CollisionDetection.hpp
+++ Game/CollisionDetection.hpp
@@ -3,3 +3,5 @@
 #include "Collision.hpp"
 #include "Collidable.hpp"
 #include "CollisionGroup.hpp"
+
+/** @brief There must be a blank line at the end-of-file. Else the compilation will throw an 'unexpected EOF' error. */
