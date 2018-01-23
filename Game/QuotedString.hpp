5de3438c8148c6b98b79df9a08b73ea4dcf3bceb
Julian van Doorn <gigajulian@gmail.com>
2018-01-23 22:37:17 +0100

    Added Doxygen comments

--- Game/QuotedString.hpp
+++ Game/QuotedString.hpp
@@ -6,11 +6,47 @@
 
 #include <fstream>
 
+/**
+ * @class	QuotedString
+ *
+ * @brief	String subclass that overloads operator>>(std::istream&, QuotedString).
+ *
+ * @author	Julian
+ * @date	2018-01-23
+ */
+
 class QuotedString : public std::string {
 public:
 	QuotedString() : std::string() { }
 	QuotedString(std::string str) : std::string(std::move(str)) { }
 };
+
+/**
+ * @fn	std::istream& operator>> (std::istream& is, QuotedString& qs)
+ *
+ * @brief	Stream extraction operator
+ * 			
+ * @detailed Used for extracting a string of characters encapsulated by "...".
+ * 			 Useful for reading exact values that are space sensitive and/or unknown where they exactly end.
+ * 			 
+ * 			 Usage: @code{.cpp}
+ * 			 QuotedString quotedString;
+ * 			 std::stringstream ss("\"some Text Encapsulated By Quote Chars\"");
+ * 			 ss >> quotedString;
+ * 			 std::cout << quotedString << std::endl; // should print someTextEncapsulatedByQuoteChars WITHOUT the quotes
+ * 			 @endcode
+ * 
+ * @author	Julian
+ * @date	2018-01-23
+ *
+ * @exception	EOFException		 	Thrown when an EOF error condition occurs.
+ * @exception	QuoteMissingException	Thrown when a Quote Missing error condition occurs.
+ *
+ * @param [in,out]	is	The input stream.
+ * @param [in,out]	qs	The quoted string.
+ *
+ * @return	std::istream reference to is
+ */
 
 std::istream& operator>> (std::istream& is, QuotedString& qs) {
 	is >> SpecialCharacter::Quote;
