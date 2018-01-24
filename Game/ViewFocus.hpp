6757f0eef2287b6c00e3d32e8d24c4399327b368
Jari van Dam <jari@jarivandam.nl>
2018-01-24 16:05:31 +0100

    Added doxygen and comments for view

--- Game/ViewFocus.hpp
+++ Game/ViewFocus.hpp
@@ -71,44 +71,76 @@ public:
 	 * @author	Julian
 	 * @date	2018-01-19
 	 */
+    void unsetFocus() {
+        this->focus = nullptr;
+    }
+
+    /**
+     * @brief Set the left border from where the camera will start following the focus object.
+     * @param [in] border Distance from left side of the screen in pixels.
+     */
 	void setLeftBorder(int border){
 		leftBorder = border;
 	}
+    /**
+     *  @brief Set the right border from where the camera will start following the focus object.
+     * @param [in]  border Distance from the right side of the screen in pixels..
+     */
 	void  setRightBorder(int border){
 		rightBorder= border;
 	}
+
+    /**
+     * @brief Set the top border from where the camera will start following the focus object.
+     * @param border [in] Distance from the top of the screen in pixels.
+     */
     void setTopBorder(int border){
         topBorder = border;
     }
+
+    /**
+     * @brief Set the bottom border from where the camera will start following the focus object.
+     * @param border [in]  Distance from the bottom of the screen in pixels.
+     */
     void setBottomBorder(int border){
         bottomBorder = border;
     }
+
+    /**
+     * @brief Get the left border from where the camera will start following the focus object.
+     * @return The left border in pixels.
+     */
 	int getLeftBorder(){
 		return leftBorder;
 	}
+    /**
+     * @brief Get the right border from where the camera will start following the focus object.
+     * @return The right border in pixels.
+     */
 	int getRightBorder(){
 		return rightBorder;
 	}
+    /**
+     * @brief Get the top border from where the camera will start following the focus object.
+     * @return The top border in pixels.
+     */
     int getTopBorder(){
         return topBorder;
     }
+    /**
+     * @brief Get the bottom border from where the camera will start following the focus object.
+     * @return The bottom border in pixels.
+     */
     int getBottomBorder(){
         return  bottomBorder;
     }
-	void unsetFocus() {
-		this->focus = nullptr;
-	}
 
-	/**
-	 * @fn	void ViewFocus::update()
-	 *
-	 * @brief	Updates this object
-	 *
-	 * @author	Julian
-	 * @date	2018-01-19
-	 */
 
+    /**
+     *  @brief Update the view by checking if the focus object is ouside of the set borders, if focus object is outside of the border the view will be moved by the difference between the view and the focus object.
+     */
 	void update() {
+        // Check if focus is defined,  it can be undefined  if state is changing for example.
 		if (focus != nullptr) {
 			// Check if focus object is outside of border and how much it is outside the border. Move view the delta of border and focus object position.
 			float leftDiff = (focus->getPosition().x - (view.getCenter().x - (view.getSize().x / 2) + leftBorder));
@@ -116,6 +148,7 @@ public:
 			float topDiff = (focus->getPosition().y - (view.getCenter().y + (view.getSize().y / 2) - topBorder));
 			float bottomDiff = (focus->getPosition().y - (view.getCenter().y - (view.getSize().y / 2) + bottomBorder));
 
+            // Check if ouside of border
 			if (leftDiff < 0 || rightDiff > 0 || topDiff < 0 || bottomDiff > 0) {
 				if (leftDiff < 0) {
 					view.move(leftDiff, 0.0);
@@ -130,6 +163,7 @@ public:
 					view.move(0.0, bottomDiff);
 				}
 			}
+            // Set default view if no focus object is defined.
 		} else {
 			view.setCenter(window.getDefaultView().getCenter());
 		}
