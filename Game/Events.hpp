1aaa48920569ae3f7c6d13931fb6a76535001088
Julian van Doorn <gigajulian@gmail.com>
2018-01-25 21:07:29 +0100

    Added EventConnectionVector to Events.hpp

--- Game/Events.hpp
+++ Game/Events.hpp
@@ -2,6 +2,9 @@
 
 #include "EventConnection.hpp"
 #include "EventSource.hpp"
+#include "EventConnectionVector.hpp"
 #include "Game.hpp"
 #include "Keyboard.hpp"
 #include "Mouse.hpp"
+
+/** @brief There must be a blank line at the end-of-file. Else the compilation will throw an 'unexpected EOF' error. */
